e318eed6a0cb39ded43a9473cc544ec4496d7ab8
arunc89 <arunkumarc2010@gmail.com>
2023-04-13 05:58:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,13 @@ resource "aws_instance" "ec2_public" {
     ami = var.ami_id
     instance_type = var.instance_type
     key_name = var.key_name
-    security_groups = ["${aws_security_group.nishant-ssh-security-group.id}"]
+    security_groups = ["${aws_security_group.arun-ssh-security-group.id}"]
     subnet_id = aws_subnet.public-subnet-1.id
     associate_public_ip_address = true
     lifecycle {
       create_before_destroy = true
     }
     tags ={
-        Name = "Nishant-ec2-Public"
+        Name = "arun-ec2-Public"
     }
 }
-
